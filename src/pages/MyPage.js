00e7a168d4bb1e7828530bc4a24f6f822539d4ad
vdmswlv108 <59062123+vdmswlv108@users.noreply.github.com>
2020-02-02 23:17:03 +0900

    eunji work start

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,33 +1,39 @@
 import React, {Component} from 'react';
-import {Image, View, SafeAreaView, Text, TextInput, Dimensions} from 'react-native';
-import {inject, observer} from "mobx-react";
-import { Container, Content } from 'native-base';
+import {
+  Image,
+  View,
+  SafeAreaView,
+  Text,
+  TextInput,
+  Dimensions,
+} from 'react-native';
+import {inject, observer} from 'mobx-react';
+import {Container, Content} from 'native-base';
 
 @inject('userStore')
 @observer
 export default class MyPage extends Component {
-    static navigationOptions = {
-        header: null,
+  static navigationOptions = {
+    header: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: null,
+      password: null,
     };
+    this.window = Dimensions.get('window');
+    this.inputWidth = this.window.width - 40;
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            email: null,
-            password: null
-        };
-        this.window = Dimensions.get('window');
-        this.inputWidth = this.window.width - 40
-    }
-
-
-    render(){
-        return (
-            <SafeAreaView style={{flex:1}}>
-                <Container style={{justifyContent:'center', alignItems:'center'}}>
-                    <Text>앙기모띠</Text>
-                </Container>
-            </SafeAreaView>
-        )
-    }
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <Container style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Text>강응디띠</Text>
+        </Container>
+      </SafeAreaView>
+    );
+  }
 }
